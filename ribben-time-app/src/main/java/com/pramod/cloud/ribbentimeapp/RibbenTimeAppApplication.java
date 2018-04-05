cb7a14a2e2a686ca55c1adeb6d34b49b2adf303f
e067411 <pramod.jangidi@mastercard.com>
2018-04-05 14:14:10 -0400

    first commit

---
+++ ribben-time-app/src/main/java/com/pramod/cloud/ribbentimeapp/RibbenTimeAppApplication.java
@@ -0,0 +1,40 @@
+package com.pramod.cloud.ribbentimeapp;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.pramod.cloud.ribbentimeapp.config.RibbonTimeConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.inject.Inject;
+
+@SpringBootApplication
+@RestController
+@RibbonClient(name = "time-service", configuration = RibbonTimeConfig.class)
+public class RibbenTimeAppApplication {
+
+	@Inject
+	private RestTemplate restTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbenTimeAppApplication.class, args);
+	}
+
+	@GetMapping
+	public String getTime(){
+		return restTemplate.getForEntity("http://time-service",String.class).getBody();
+
+	}
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ ribben-time-service/src/test/java/com/pramod/cloud/ribbentimeservice/RibbenTimeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pramod.cloud.ribbentimeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RibbenTimeServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
